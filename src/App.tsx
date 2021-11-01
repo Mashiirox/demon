c18f75dee3196dbf64033d473b299623f5cd7d67
Mashiirox <Mashiirox@foxmail.com>
2021-11-01 14:45:08 +0800

    feat: Add form-input new style like element login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button } from 'antd';
+import { useState } from 'react';
+import { Button, Modal } from 'antd';
 
 import './App.less';
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <Button className='border-none'>HELLO!</Button>
+    <div className='flex justify-center'>
+      <Button onClick={() => setShow(true)}>HELLO!</Button>
+      <Modal
+        visible={show}
+        onOk={() => setShow(false)}
+        onCancel={() => setShow(false)}
+      >
+        <div className='form-username m-16'>
+          <input type='text' placeholder='用户名' className='form-username-input' />
+          <label className='form-username-label text-gray-400'>用户名</label>
+        </div>
+      </Modal>
     </div>
   );
 }
